b03061e30378d4c1f6f0638eb1745ced2bf27e99
xiaochengzuo <keith527@yeah.net>
2017-03-13 17:13:03 +0800

    fix bug

--- index.android.js
+++ index.android.js
@@ -3,6 +3,9 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-App
+import {
+   AppRegistry,
+ } from 'react-native';
+ import App from './App';
 
 AppRegistry.registerComponent('InstantMessage', () => App);
